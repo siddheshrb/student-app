abcd7608b0fe8ecac8568c695a9397256740ac7c
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-04-10 20:24:08 +0530

    Corrected DB configs and updated Student as an entity

--- src/main/java/com/eternity/studentapp/model/Student.java
+++ src/main/java/com/eternity/studentapp/model/Student.java
@@ -2,11 +2,24 @@ package com.eternity.studentapp.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
