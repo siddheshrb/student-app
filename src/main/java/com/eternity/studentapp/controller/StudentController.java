21f10b8339ea1f3cf087c693aa988c8ceeb4d20b
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-04-11 18:03:21 +0530

    Implemented add new student

--- src/main/java/com/eternity/studentapp/controller/StudentController.java
+++ src/main/java/com/eternity/studentapp/controller/StudentController.java
@@ -3,9 +3,7 @@ package com.eternity.studentapp.controller;
 import com.eternity.studentapp.model.Student;
 import com.eternity.studentapp.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,11 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 
 
--- src/main/java/com/eternity/studentapp/service/StudentService.java
+++ src/main/java/com/eternity/studentapp/service/StudentService.java
@@ -2,6 +2,7 @@ package com.eternity.studentapp.service;
 
 import com.eternity.studentapp.dao.StudentRepository;
 import com.eternity.studentapp.model.Student;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+@Log4j2
 @Service
 public class StudentService {
 
@@ -23,5 +25,10 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        log.info("New student request received", student);
+        studentRepository.save(student);
     }
 }
